8dbbe328ed9a20e385cae0302d3f5f8ae0957695
linboe <linn.boek@gmail.com>
2026-02-03 16:42:25 +0100

    pseudocode - assignment

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 // @ts-nocheck
 import './style.css';
 
+/**
+ *      Ett fält för att mata in en utgift (belopp och beskrivning)
+ *      Ett fält för att mata in en inkomst (belopp och beskrivning)
+ *      Bredvid varje budgetpost ska det finnas en radera-knapp
+ *          (när utskrivet i list)
+ *      Det ska visas en balans (inkomster minus utgifter)
+ *          - anpassas vid borttagning av värde i list
+ *      Balansens ska färgkodas beroende på om det är ett positivt eller negativt värde
+ *      Balansen ska uppdateras varje gång en ny utgift eller inkomst matas in
+ *      Till varje budgetpost ska det gå att välja en kategori från en dropdown-lista (select)
+ *      Informationen ska sparas i local storage så att när användaren kommer till sidan nästa gång, så ska informationen finnas kvar. Nytt
+ *      Kategorierna ska läsas in via JSON. Vi går igenom detta på lektionen.
+ */
+
 const registerBtn = document.querySelector('#register');
 const budgetForm = document.querySelector('#budgetForm');
 const transactions = [];
