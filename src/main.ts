804618e978cf8fbab9591015e88e48bc791b1e4c
linboe <linn.boek@gmail.com>
2026-02-03 15:50:39 +0100

    start foundation html and js

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,71 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+// @ts-nocheck
+import './style.css';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const registerBtn = document.querySelector('#register');
+const transactions = [];
+const expenseAmount = document.querySelector('#expenseAmount');
+const noteExpense = document.querySelector('#noteExpense');
+const categoryExpense = document.querySelector('#categoryExpense');
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const incomeAmount = document.querySelector('#incomeAmount');
+const noteIncome = document.querySelector('#noteIncome');
+const categoryIncome = document.querySelector('#categoryIncome');
+
+const balance = document.querySelector('#balance');
+const list = document.querySelector('#list');
+
+registerBtn.addEventListener('click', registerTransaction => {
+  registerTransaction.preventDefault();
+
+  if (expenseAmount.value) {
+    transactions.push({
+      amount: Number(expenseAmount.value),
+      note: noteExpense.value,
+      category: categoryExpense.value,
+      type: 'expense',
+    });
+  }
+
+  if (incomeAmount.value) {
+    transactions.push({
+      amount: Number(incomeAmount.value),
+      note: noteIncome.value,
+      category: categoryIncome.value,
+      type: 'income',
+    });
+  }
+
+  registerTransactions();
+});
+
+function registerTransactions() {
+  list.innerHTML = '';
+
+  let totalBalance = 0;
+
+  transactions.forEach(tx => {
+    const li = document.createElement('li');
+    li.textContent = `${tx.note} ${tx.amount} kr (${tx.category})`;
+    list.appendChild(li);
+
+    if (tx.type === 'income') {
+      totalBalance += tx.amount;
+    } else {
+      totalBalance -= tx.amount;
+    }
+  });
+
+  balance.textContent = totalBalance;
+}
+
+/*
+
+function registerTransaction() {
+    registerTransaction.innerHTML +=
+}
+*/
+/* //(e) och e.preventDefault(); tillfälligt för att ej skicka iväg formuläret
+}
+function registerTransaction(e) {
+  e.preventDefault(); 
+*/
